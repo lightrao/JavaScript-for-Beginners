7e6fb0bdb4e4003de5a670d13b26990277774ee0
RAO YANG <lightrao@hotmail.com>
2023-04-03 21:15:01 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
-console.log("Hi browser :D");
+// declaring variables
+var a;
+console.log(a);
+
+// assigning variables
+a = null;
+console.log(a);
+
+// assigning again
+a = 7;
+console.log(a);
+
+// declaring and assigning
+var b = 2;
+console.log(b);
