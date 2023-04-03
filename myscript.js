1c3221a4d71e55a0acc1e50b884301b5f5344961
RAO YANG <lightrao@hotmail.com>
2023-04-03 08:16:18 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
-console.log("Hi browser :D");
+/* 
+Data Types in Javascript:
+undefined, null, boolean, string, symbol, number, object
+*/
+
+// Declare Variables
+
+// be use throughout your hole program
+var myName = "ZhangSan";
+console.log(myName);
+myName = "LiSi";
+console.log(myName);
+
+// use in scope where you declared it
+let age = 33;
+
+// never change
+const pi = 3.14;
