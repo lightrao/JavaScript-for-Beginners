e03262b25d8ab4b883864ef1ff2e4a3393465cbb
RAO YANG <lightrao@hotmail.com>
2023-04-03 21:52:02 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,15 @@
-console.log("Hi browser :D");
+// Adding Numbers
+var sum = 1 + 2;
+console.log(sum);
+
+// Subtracting Numbers
+var diffrence = 4 - 9;
+console.log(diffrence);
+
+// Multiplying Numbers
+var product = 3 * 4;
+console.log(product);
+
+// Dividing Numbers
+var quotient = 10 / 0;
+console.log(quotient);
