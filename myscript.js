f5b6ba98f417a672299e84953d9d0887420bb1c3
RAO YANG <lightrao@hotmail.com>
2023-04-04 07:39:55 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-console.log("Hi browser :D");
+// Compound Assignment with Augmented Addition
+var a = 2;
+a += 5; // a = a + 5;
+console.log(a);
+
+// Compound Assignment with Augmented Subtraction
+var b = 3;
+b -= 7; // b = b - 7;
+console.log(b);
+
+// Compound Assignment with Augmented Multiplication
+var c = 5;
+c *= 0.5; // c = c * 0.5
+console.log(c);
+
+// Compound Assignment with Augmented Division
+var d = 7;
+d /= 9; // d = d / 9
+console.log(d);
