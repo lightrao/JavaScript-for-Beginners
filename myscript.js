8eb9d99391e035afb6b5090330bfc6892cd9d7d7
RAO YANG <lightrao@hotmail.com>
2023-04-03 21:44:43 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,9 @@
-console.log("Hi browser :D");
+// case sensitivity in variables
+var myName = "ZhangSan";
+var myname = "LiSi";
+
+// camelCase
+var peopleInTheRoom = 10;
+
+// snake_case
+var people_in_the_room = 10;
