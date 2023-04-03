d01ec0e29b1be00bf9b271ef251bce3d5cf21149
RAO YANG <lightrao@hotmail.com>
2023-04-03 21:57:22 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,9 @@
-console.log("Hi browser :D");
+// Incrementing Numbers
+var myNumber = 2;
+myNumber++; // myNumber = myNumber + 1;
+console.log(myNumber);
+
+// Decrementing Numbers
+var yourNumber = 5;
+yourNumber--; // yourNumber = yourNumber - 1;
+console.log(yourNumber);
