16ddde023b6ff2f35dbb5dd2e5107e2b9862ed6c
RAO YANG <lightrao@hotmail.com>
2023-04-04 07:55:31 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,5 @@
-console.log("Hi browser :D");
+// Quoting Strings with Single Quotes
+var myString = 'I\'m a "double quote".';
+console.log(myString);
+var anotherString = `I'm a "double qoute".`;
+console.log(anotherString);
