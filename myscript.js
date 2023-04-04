bc93f1273e8eba6f9147657d8e8f8ee77e6ee9a9
RAO YANG <lightrao@hotmail.com>
2023-04-04 08:12:38 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
-console.log("Hi browser :D");
+// Escape Sequences
+/* 
+Code   Output
+\'     single quote
+\"     double quote
+\\     backslash
+\n     newline
+\r     carrige return
+\t     tab
+*/
+var myString = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myString);
