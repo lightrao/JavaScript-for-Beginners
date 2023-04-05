08aacd08db01bf0600d907989d2c3340c351d66a
RAO YANG <lightrao@hotmail.com>
2023-04-06 07:20:44 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,5 @@
-console.log("Hi browser :D");
+// Plus Operator
+var ourString = "Doubt is the " + "key to knowledge.";
+console.log(ourString);
+var myString = "Bad times " + "make a " + "good man.";
+console.log(myString);
