79929b91b6c3ff57f0c8144ee8bb692ce5686582
RAO YANG <lightrao@hotmail.com>
2023-04-07 07:46:54 +0800

    find last letter

--- myscript.js
+++ myscript.js
@@ -2,3 +2,6 @@
 var myString = "012345";
 var thirdLetter = myString[2];
 console.log(thirdLetter);
+// Find the Last Character in String
+var lastLetter = myString[myString.length - 1];
+console.log(lastLetter);
