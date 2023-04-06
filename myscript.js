73f2c2f5f374be1a5525fd7c9d5f36ee3d528909
RAO YANG <lightrao@hotmail.com>
2023-04-07 07:50:03 +0800

    nth to last char

--- myscript.js
+++ myscript.js
@@ -5,3 +5,6 @@ console.log(thirdLetter);
 // Find the Last Character in String
 var lastLetter = myString[myString.length - 1];
 console.log(lastLetter);
+// Find Nth-to-Last Character in String
+var secondToLastLetter = myString[myString.length - 2];
+console.log(secondToLastLetter);
