74ec706b7b9b0e264a3d0a89394dc14484d27b6e
RAO YANG <lightrao@hotmail.com>
2023-04-07 07:28:10 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,8 @@
-console.log("Hi browser :D");
+// Bracket Notation to Find Character in String
+var myString = "abcde";
+var firstLetter = myString[0];
+console.log(firstLetter);
+var secondLetter = myString[1];
+console.log(secondLetter);
+var lastLetter = myString[myString.length - 1];
+console.log(lastLetter);
