24e592fea5d7508636cf0abfd77f0854f0744936
RAO YANG <lightrao@hotmail.com>
2023-04-07 07:34:16 +0800

    done

--- myscript.js
+++ myscript.js
@@ -1,1 +1,6 @@
-console.log("Hi browser :D");
+// Understand String Immutability
+var myString = "Kello World!";
+// myString[0] = "H";
+// console.log(myString);
+myString = "Hello World!";
+console.log(myString);
